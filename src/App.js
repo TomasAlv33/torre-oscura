3165c43742988e0b98d9c981153b820946e0dd3f
TomasAlv33 <tomas.agustin.alvarez@hotmail.com>
2021-10-13 22:07:48 -0300

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './Components/NavBar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavBar/>
+        <img src={logo} className="App-logo" alt="logo" />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
